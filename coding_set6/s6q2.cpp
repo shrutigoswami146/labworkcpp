be3deb26f80225c83ecc89a890929645cf533f94
Shruti Goswami <goswamishruti405@gmail.com>
2025-12-23 01:19:35 +0530

    Add files via upload

---
+++ coding_set6/s6q2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class base{
+    public:
+    base(){
+        cout<<"Base constructor called."<<endl;
+    }
+    ~base(){
+        cout<<"Base destructor called."<<endl;
+    }
+};
+class derived : public base{
+    public:
+    derived(){
+        cout<<"derived constructor called."<<endl;
+    }
+    ~derived(){
+        cout<<"derived destructor called."<<endl;
+    }
+};
+int main(){
+    
+    derived d1;
+    
+}
